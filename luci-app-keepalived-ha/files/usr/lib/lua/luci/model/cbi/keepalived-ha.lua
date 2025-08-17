ec685734ebd4c2395899e7c57de1723ecfd599b9
CoderX <962129122@qq.com>
2025-08-17 21:41:37 +0800

    bug-2

--- luci-app-keepalived-ha/files/usr/lib/lua/luci/model/cbi/keepalived-ha.lua
+++ luci-app-keepalived-ha/files/usr/lib/lua/luci/model/cbi/keepalived-ha.lua
@@ -84,7 +84,6 @@ control_openclash.rmempty = false
 main = m:section(NamedSection, "main", "main", translate("主路由设置"),
     translate("仅当角色为“主路由”时生效的配置参数"))
 main.anonymous = true
-main:depends("role", "main")  -- 新增：仅当角色为主路由时显示
 
 peer_ip = main:option(Value, "peer_ip", translate("备路由IP地址"))
 peer_ip.datatype = "ip4addr"
@@ -118,7 +117,6 @@ check_interval:depends("role", "main")
 peer = m:section(NamedSection, "peer", "peer", translate("备路由设置"),
     translate("仅当角色为“备路由”时生效的配置参数"))
 peer.anonymous = true
-peer:depends("role", "peer")  -- 新增：仅当角色为备路由时显示
 
 main_ip = peer:option(Value, "main_ip", translate("主路由IP地址"))
 main_ip.datatype = "ip4addr"
