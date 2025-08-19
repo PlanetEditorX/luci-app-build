d91d32d3a9cdbb7e3badc52e8efd62bb871a2e64
CoderX <962129122@qq.com>
2025-08-19 11:43:17 +0800

    test

--- luci-app-keepalived-ha/files/etc/keepalived/template/vip_up.sh
+++ luci-app-keepalived-ha/files/etc/keepalived/template/vip_up.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 VIP="@VIP@"
 IFACE="@INTERFACE@"
+logger -t "keepalived-ha-vip_up" "启动VIP绑定..."
 sh /etc/keepalived/vip_manager.sh "$VIP" "$IFACE" bind
+logger -t "keepalived-ha-vip_up" "VIP绑定完成"
